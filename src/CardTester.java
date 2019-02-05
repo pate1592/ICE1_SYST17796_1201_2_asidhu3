9642f06afb07614ff72046c83186ac32b07b2e15
asidhu3 <>
2019-02-04 20:38:57 -0500

    First commit

---
+++ src/CardTester.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author test
+ */
+public class CardTester {
+    public static void main(String[] args)
+    {
+        Card c1=new Card("Clubs",6);
+        c1.setSuit("Hearts");
+        System.out.println ("New suit is "+ c1.getSuit());
+        
+    }
+}
